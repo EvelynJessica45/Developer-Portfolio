5b01355d3b8331933fadfe45dfe5089e2125f34b
Evelyn_Jessica <156339427+EvelynJessica45@users.noreply.github.com>
2025-03-28 16:08:22 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,19 +1,25 @@
 export const experiences = [
-  
   {
     id: 1,
-    title: "FullStack Developer/Tester",
-    company: "Uber (freelance)",
-    duration: "(Jun 2025 Jan - Present)"
+    title: "Node JS Developer",
+    company: "Sikoter",
+    duration: "(Mar 2025 - Present)"
   },
+  
   {
     id: 2,
+    title: "FullStack Developer/Tester",
+    company: "Uber (freelance)",
+    duration: "(Jun 2025 - Present)"
+  },
+  {
+    id: 3,
     title: 'Research Intern',
     company: "Pravinya Infotech",
     duration: "(Feb 2024 - June 2024)"
   },
   {
-    id: 3,
+    id: 4,
     title: "Self Employed",
     company: "Code and build something in everyday.",
     duration: "(Jan 2022 - Present)"
